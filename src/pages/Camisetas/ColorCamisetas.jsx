eaf01df8e8b8b15d97d3d4cb59417b2811ad0db0
Mateor13 <mateo.torres@epn.edu.ec>
2025-04-02 02:28:37 -0500

    Agregar nueva imagen y mejorar la presentación del formulario en ColorCamisetas

--- src/pages/Camisetas/ColorCamisetas.jsx
+++ src/pages/Camisetas/ColorCamisetas.jsx
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import KFC from "../../../public/images/logo.png";
 import ARP from "../../../public/images/ARP.jpg";
+import Tropi from "../../../public/images/tropi.png";
 
 const ColorCamisetas = () => {
     const [form, setForm] = useState({ fecha: "" });
@@ -16,6 +17,23 @@ const ColorCamisetas = () => {
 
     const fechaFormateada = (fecha) => {
         if (!fecha) return "";
+        const mensaje = document.getElementById("mensaje");
+        const fechaActual = new Date().toISOString().split("T")[0];
+        if (fecha < fechaActual) {
+            mensaje.classList.remove("text-green-500");
+            mensaje.classList.add("text-red-500");
+            const [year, month, day] = fecha.split("-").map(Number);
+            const fechaLocal = new Date(year, month - 1, day);
+            const fechaFormateada = fechaLocal.toLocaleDateString("es-ES", {
+                weekday: "long",
+                day: "2-digit",
+                month: "long",
+                year: "numeric",
+            });
+            return fechaFormateada;
+        }
+        mensaje.classList.remove("text-red-500");
+        mensaje.classList.add("text-green-500");
         const [year, month, day] = fecha.split("-").map(Number);
         const fechaLocal = new Date(year, month - 1, day);
         const fechaFormateada = fechaLocal.toLocaleDateString("es-ES", {
@@ -44,7 +62,10 @@ const ColorCamisetas = () => {
             }
         } catch (error) {
             console.error(error);
-            toast.error(error?.response.data.error)
+            const mensaje = document.getElementById("mensaje");
+            mensaje.innerHTML = error?.response.data.error;
+            mensaje.classList.remove("text-green-500");
+            mensaje.classList.add("text-red-500");
         }
     };
 
@@ -52,28 +73,30 @@ const ColorCamisetas = () => {
         <div className="w-full min-h-screen bg-red-700 flex flex-col justify-center items-center" >
                 <ToastContainer />
                 <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
-                <small className="text-black-700 text-center block my-4 text-lg">Color de Camiseta</small>
+                <small className="text-black-700 text-center block my-4 text-3xl font-extrabold">Color de Camiseta</small>
                 <div className="flex justify-center items-center gap-4 mb-4">
-                    <img src={KFC} alt="Logo" className="w-1/2 rounded-lg shadow-md" />
+                    <img src={KFC} alt="Logo" className="w-1/3 rounded-lg shadow-md" />
                     <img src={ARP} alt="Logo" className="w-1/3 rounded-lg shadow-md" />
+                    <img src={Tropi} alt="Logo" className="w-1/3 rounded-lg shadow-md" />
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                    <label className="mb-2 block text-sm font-semibold">Seleccione una Fecha para Visualizar el Color de la Camiseta:</label>
+                    <label className="mb-2 block text-sm font-semibold">Seleccione una fecha a averiguar:</label>
                     <input
                         type="date"
-                        className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                        className="block w-full rounded-md border border-gray-300 focus:border-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-700 py-1 px-2 text-gray-500"
                         min={new Date().toISOString().split("T")[0]} 
                         name="fecha"
                         value={form.fecha}
                         onChange={handleChange}
+                        placeholder="Fecha"
                     />
                     </div>
-                    <small className="text-center justify-center text-green-500 text-sm mb-2 block">
+                    <small id="mensaje" className="text-center justify-center text-green-500 text-sm mb-2 block">
                         {fechaFormateada(form.fecha)}
                     </small>                    
-                    <small className="text-gray-500 text-sm mb-4">*Recuerda que la fecha no puede ser anterior a la actual.</small><br/><br/>
-                    <button type="submit" className="py-2 w-full block text-center bg-gray-700/100 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Visualizar el Color de Camiseta</button>
+                    <small className="text-gray-500 text-sm mb-4">*Recuerda que la fecha es obligatoria y no puede ser anterior a la actual.</small><br/><br/>
+                    <button type="submit" className="py-2 w-full block text-center bg-blue-700/100 text-white border rounded-xl hover:scale-100 duration-300 hover:bg-blue-900 hover:text-white">Visualizar el Color de Camiseta</button>
                 </form>
             </div>
         </div>
