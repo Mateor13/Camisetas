a4402b3f7d12c5eccc196e7fd75ea610fd35fdde
Mateor13 <mateo.torres@epn.edu.ec>
2025-04-02 00:03:09 -0500

    Formatear la fecha en los componentes Azul, Gris y Roja para mejorar la presentación

--- src/pages/Gris.jsx
+++ src/pages/Gris.jsx
@@ -4,6 +4,12 @@ import gris from "../../public/images/gris.webp";
 const Gris = () => {
     const navigate = useNavigate();
     const fecha = localStorage.getItem("fecha");
+    const fechaFormateada = new Date(fecha).toLocaleDateString("es-ES", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+    });
 
     const handleClick = () => {
         navigate("/");
@@ -12,9 +18,9 @@ const Gris = () => {
     return (
         <div className="w-full min-h-screen bg-gray-600 flex flex-col justify-center items-center">
             <h1 className="text-white text-4xl font-bold mb-4">Gris</h1>
-            <p className="text-white text-lg mb-4"> La camiseta del día de hoy </p>
+            <p className="text-white text-lg mb-4"> La camiseta de este día:</p>
+            <p className="text-white text-lg mb-4">{fechaFormateada}</p>
             <img src={gris} alt="Camiseta Roja" className="mb-4 rounded-lg shadow-lg w-64 h-auto" />
-            <p className="text-white text-lg mb-4">{fecha}</p>
             <button
                 onClick={() => {
                     localStorage.removeItem("fecha");
--- src/pages/Roja.jsx
+++ src/pages/Roja.jsx
@@ -4,6 +4,12 @@ import roja from "../../public/images/roja.jpg";
 const Roja = () => {
     const navigate = useNavigate();
     const fecha = localStorage.getItem("fecha");
+    const fechaFormateada = new Date(fecha).toLocaleDateString("es-ES", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+    });
 
     const handleClick = () => {
         navigate("/");
@@ -12,9 +18,9 @@ const Roja = () => {
     return (
         <div className="w-full min-h-screen bg-red-700 flex flex-col justify-center items-center">
             <h1 className="text-white text-4xl font-bold mb-4">Roja</h1>
-            <p className="text-white text-lg mb-4"> La camiseta del día de hoy </p>
+            <p className="text-white text-lg mb-4"> La camiseta de este día:</p>
+            <p className="text-white text-lg mb-4">{fechaFormateada}</p>
             <img src={roja} alt="Camiseta Roja" className="mb-4 rounded-lg shadow-lg w-64 h-auto" />
-            <p className="text-white text-lg mb-4">{fecha}</p>
             <button
                 onClick={() => {
                     localStorage.removeItem("fecha");
