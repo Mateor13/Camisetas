ae0a9ad42c66bd2ef4c7c2b883c16ac9b6e7e41e
Mateor13 <mateo.torres@epn.edu.ec>
2025-04-01 23:30:46 -0500

    Eliminar archivo de configuración de Vercel y actualizar el componente ColorCamisetas para incluir imágenes y mejorar la interfaz de usuario

--- src/pages/Camisetas/ColorCamisetas.jsx
+++ src/pages/Camisetas/ColorCamisetas.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import KFC from "../../../public/images/logo.png";
+import ARP from "../../../public/images/ARP.jpg";
 
 const ColorCamisetas = () => {
     const [form, setForm] = useState({ fecha: "" });
@@ -38,22 +40,26 @@ const ColorCamisetas = () => {
                 <ToastContainer />
                 <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
                 <small className="text-black-700 text-center block my-4 text-lg">Color de Camiseta</small>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                        <label className="mb-2 block text-sm font-semibold">Fecha:</label>
-                        <input
-                            type="date"
-                            className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
-                            min={new Date().toISOString().split("T")[0]} 
-                            name="fecha"
-                            value={form.fecha}
-                            onChange={handleChange}
-                        />
-                        </div>
-                        
-                        <button type="submit" className="py-2 w-full block text-center bg-gray-700/100 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Averiguar Camiseta</button>
-                    </form>
+                <div className="flex justify-center items-center gap-4 mb-4">
+                    <img src={KFC} alt="Logo" className="w-1/2 rounded-lg shadow-md" />
+                    <img src={ARP} alt="Logo" className="w-1/3 rounded-lg shadow-md" />
                 </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                    <label className="mb-2 block text-sm font-semibold">Seleccione una fecha para visualizar el color de la camiseta:</label>
+                    <input
+                        type="date"
+                        className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                        min={new Date().toISOString().split("T")[0]} 
+                        name="fecha"
+                        value={form.fecha}
+                        onChange={handleChange}
+                    />
+                    </div>
+                    
+                    <button type="submit" className="py-2 w-full block text-center bg-gray-700/100 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Averiguar Camiseta</button>
+                </form>
+            </div>
         </div>
     );
 };
